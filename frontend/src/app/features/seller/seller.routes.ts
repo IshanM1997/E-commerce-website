02405edd539b8dbebda7faebeb21f14cc842d939
Ishan Mukhopadhyay <ishanmukherjee66@gmail.com>
2026-06-15 13:33:13 +0530

    Add files via upload

---
+++ frontend/src/app/features/seller/seller.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SellerDashboardComponent } from './components/seller-dashboard.component';
+
+export const SELLER_ROUTES: Routes = [
+  { path: '', component: SellerDashboardComponent }
+];
