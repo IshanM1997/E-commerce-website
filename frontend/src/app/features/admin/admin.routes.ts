6ddb433b21afc640eee21d3d078d42eff7c1f822
Ishan Mukhopadhyay <ishanmukherjee66@gmail.com>
2026-06-15 13:25:08 +0530

    Create admin.routes.ts

---
+++ frontend/src/app/features/admin/admin.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard.component';
+
+export const ADMIN_ROUTES: Routes = [
+  { path: '', component: AdminDashboardComponent }
+];
